dec3c1cbbde6707902e839f936d25d6cea765ef4
Max Kofler <max.kofler@danfoss.com>
2026-06-18 13:41:26 +0200

    [TMP] Backup

---
+++ hdl/MAC/MAC_TB.vhd
@@ -0,0 +1,60 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library modmac;
+use modmac.MAC;
+
+entity MAC_tb is
+end;
+
+architecture bench of MAC_tb is
+  -- Clock period
+  constant clk_period : time := 5 ns;
+  -- Generics
+  -- Ports
+  signal xii_i_c : std_logic                    := '0';
+  signal xii_i_d : std_logic_vector(7 downto 0) := (others => '0');
+  signal xii_i_e : std_logic                    := '0';
+  signal xii_i_r : std_logic;
+begin
+
+  MAC_inst : entity MAC
+    port map
+    (
+      xii_i_c => xii_i_c,
+      xii_i_d => xii_i_d,
+      xii_i_e => xii_i_e,
+      xii_i_r => xii_i_r
+    );
+
+  xii_i_c <= not xii_i_c after clk_period/2;
+
+  process begin
+    -- IPG
+    xii_i_e <= '0';
+    for byte in 0 to 11 loop
+      wait until falling_edge(xii_i_c);
+    end loop;
+
+    -- Preamble
+    xii_i_e <= '1';
+    xii_i_d <= x"55";
+    for byte in 0 to 6 loop
+      wait until falling_edge(xii_i_c);
+    end loop;
+
+    -- SFD
+    xii_i_d <= x"D5";
+    wait until falling_edge(xii_i_c);
+
+    -- Some Data
+    xii_i_d <= x"AA";
+    for byte in 0 to 6 loop
+      wait until falling_edge(xii_i_c);
+    end loop;
+
+  end process;
+
+end;
---
+++ test.py
@@ -0,0 +1,23 @@
+from vunit import VUnit
+
+# Create VUnit instance by parsing command line arguments
+vu = VUnit.from_argv()
+
+# Optionally add VUnit's builtin HDL utilities for checking, logging, communication...
+# See http://vunit.github.io/hdl_libraries.html.
+vu.add_vhdl_builtins()
+# or
+# vu.add_verilog_builtins()
+
+# Create library 'lib'
+lib = vu.add_library("modmac")
+
+# Add all files ending in .vhd in current working directory to library
+lib.add_source_files("hdl/MII/RGMII/DDRAltera.vhd")
+lib.add_source_files("hdl/MII/RGMII/RGMIIPhy_TB.vhd")
+lib.add_source_files("hdl/MII/RGMII/RGMIIPhy.vhd")
+
+# Run vunit function
+vu.main()
+
+
